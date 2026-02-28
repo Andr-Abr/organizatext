db69f9e5c19523d67f9823f04ff20d506beafc02
Andr-Abr <1218236@gmail.com>
2026-02-28 05:17:54 -0500

    Fix Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.origins_list,
+    allow_origins=["https://organizatext-web.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
